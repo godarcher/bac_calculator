c54fa4a5535c415dcff41c3f865a6ecca7954f58
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-05 12:50:45 +0100

    new updates

--- calculate_bac.py
+++ calculate_bac.py
@@ -2,3 +2,9 @@ class bac_finder(object):
     def __init__(self, myweight, timepassed, volume, amount, gender):
         self.myweight = myweight
         self.timepassed = timepassed 
+        self.volume = volume
+        self.amount = amount 
+        self.gender = gender
+        self.std = 0.0068
+
+    def normal_drink
