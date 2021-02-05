a89cc28183d11a1b3b1d553d1f3ec0595cf2e755
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-05 12:56:11 +0100

    added woman promille function

--- calculate_bac.py
+++ calculate_bac.py
@@ -1,4 +1,6 @@
 class bac_finder(object):
+
+    #the initiation, also containing std
     def __init__(self, myweight, timepassed, volume, amount, gender):
         self.myweight = myweight
         self.timepassed = timepassed 
@@ -7,8 +9,14 @@ class bac_finder(object):
         self.gender = gender
         self.std = 0.0068
 
+    #this function calculates the rouned value of a normal drink
     def normal_drink(self):
         return round((self.std * self.volume) * self.amount, 2)
         
+    #this function calculates the promillage of a man
     def man_prom(self):
         return round((self.normal_drink() * 12) / ((self.myweight * 1.7) - (0.15 * self.time)), 2)
+
+    #this function calculates teh promillage of a woman
+    def woman_prom(self):
+        return round((self.normal_drink() * 12) / ((self.myweight * 1.6) - (0.15 * self.time)), 2)
